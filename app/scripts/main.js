6d76c742a08eb7552cdec61521d061bcf2956c7b
ChrisLMerrill <chris@ide4selenium.com>
2019-03-27 16:04:19 -0400

    add some debugging statements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,15 +5,19 @@ import {Store} from 'webext-redux';
 import PageList from '../components/PageList';
 import AddPageForm from '../components/AddPageForm';
 
-console.log('Main UI window displayed.');
+console.log('Pop-up script is running...');
 
 export function addPage(payload) {
     return { type: "add-page", payload:payload};
 }
 
+console.log('connecting to the store...');
 const store = new Store();
+console.log('store is connected.');
 
 const unsubscribe = store.subscribe(() => {
     unsubscribe();
     render(<Provider store={store}><PageList/><AddPageForm/></Provider>, document.getElementById("root"));
 });
+
+console.log('Popup script is done.');
